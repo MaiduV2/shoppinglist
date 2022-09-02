e70fa2ac21051bdb0aaeff8f43f2caa4816e6b92
GertMariusAndreas Maidla <MM-20-854@tptlive.ee>
2022-09-02 15:43:16 +0300

    posts

--- add.php
+++ add.php
@@ -1,11 +1,24 @@
 <?php
+
+if(trim($_POST['title']) && trim($_POST['body'])) {
+    $posts = [];
+    if (file_exists('posts.json')) {
+    $json = file_get_contents('posts.json');
+    $posts = json_decode($json, true);
+    }
+    $posts[] = [
+        'title' => $_POST['title'],
+        'body' => $_POST['body'],
+    ];
+    $json = json_encode($posts);
+$file = fopen('posts.json', 'w');
 /*
 a - append
 w - write
 r - read
 */ 
-$file = fopen('list.txt', 'a');
-fwrite($file, $_POST['newitem'] . "\n");
+fwrite($file, $json);
 fclose($file);
+}
 header('Location: /index.php');
 die();
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-$content = file_get_contents('list.txt');
-$items = explode("\n", $content);
-array_pop($items);
+$json = file_get_contents('posts.json');
+$posts = json_decode($json, true);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,19 +8,42 @@ array_pop($items);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="/add.php" method="POST">
-        <input type="text" name="newitem">
-        <input type="submit" value="add">
-    </form>
-    <ul>
-        <?php foreach($items as $item): ?>
-            <li>
-                <?=$item?>
-            </li>
+    <div class="container">
+        <a class="button is-primary" href="/new.php">New Post</a>
+            <ul>
+                <?php foreach($posts as $post): ?>
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                            <?=$post['title']?>
+                    </p>
+                        <button class="card-header-icon" aria-label="more options">
+                            <span class="icon">
+                                <i class="fas fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                        </button>
+                </header >
+            <div class="card-content">
+            <?=$post['body']?>
+            <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                  <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+            </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">Save</a>
+    <a href="#" class="card-footer-item">Edit</a>
+    <a href="#" class="card-footer-item">Delete</a>
+  </footer>
+</div>
         <?php endforeach; ?>
     </ul>    
+    </div>
 </body>
 </html>
